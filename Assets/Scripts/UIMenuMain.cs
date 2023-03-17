129d5b3adbdfd05b02e798cb86e3fd5cf346eed0
Kyle <kyle.ng7512@gmail.com>
2023-03-17 16:25:08 -0700

    Add relay code

--- Assets/Scripts/UIMenuMain.cs
+++ Assets/Scripts/UIMenuMain.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Unity.Netcode;
 
 public class UIMenuMain : MonoBehaviour
 {
@@ -12,13 +13,31 @@ public class UIMenuMain : MonoBehaviour
         Application.Quit();
     }
 
-    public void HostLobby()
+    public async void HostLobby()
     {
         Debug.Log("Hosting Lobby");
+        // this allows the UnityMultiplayer and UnityMultiplayerRelay scene to work with and without
+        // relay features - if the Unity transport is found and is relay protocol then we redirect all the 
+        // traffic through the relay, else it just uses a LAN type (UNET) communication.
+        if (RelayManager.Instance.IsRelayEnabled)
+            await RelayManager.Instance.SetupRelay();
+
+        if (NetworkManager.Singleton.StartHost())
+        {
+            Debug.Log("Host started...");
+            //activateRacingUI();
+            //enableRaceScene();
+        }
+        else
+        {
+            Debug.Log("Unable to start host...");
+        }
+
     }
 
-    public void JoinLobby()
+    public async void JoinLobby()
     {
         Debug.Log("Joinging Lobby");
+        NetworkManager.Singleton.StartClient();
     }
 }
