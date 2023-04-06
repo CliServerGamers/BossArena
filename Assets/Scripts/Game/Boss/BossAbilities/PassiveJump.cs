c316760b52e2dd4c725570c5eb70b4b1a7ea6e0f
Clayton Hunter <claytonhunter10@gmail.com>
2023-04-06 13:11:01 -0700

    Implemented boss sound effects

--- Assets/Scripts/Game/Boss/BossAbilities/PassiveJump.cs
+++ Assets/Scripts/Game/Boss/BossAbilities/PassiveJump.cs
@@ -43,7 +43,7 @@ namespace BossArena.game
             }
             boss.GetComponent<Animator>().SetBool("isJumping", true);
             boss.GetComponent<Animator>().SetBool("isAttacking", false);
-            boss.PlaySound("vine-boom", 5.0f, 10.0f);
+            boss.PlaySound("boss-passive-jump", 2.0f, 2.0f);
 
             switch (passiveJumpState)
             {
